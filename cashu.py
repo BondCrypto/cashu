2d94b6bf265587df36d6f91ccb5b15a6da8d18ea
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-01-04 17:13:57 +1100

    first working version

---
+++ cashu.py
@@ -0,0 +1,68 @@
+import datetime
+from datetime import date
+import time
+import sys
+
+class Cashflow(object):
+	def __init__(self, name, amount, howOftenReoccur, startDate, endDate):
+		self.name = name
+		self.amount = amount
+		self.howOftenReoccur = howOftenReoccur # timedelta interval
+		self.startDate = startDate
+		self.endDate = endDate
+
+class OneoffCashflow(object):
+	def __init__(self, desc, amount, date):
+		self.desc = desc
+		self.amount = amount
+		self.date = date
+
+	def __repr__(self):
+		return self.desc + ' $'+str(self.amount) + ' ' + str(self.date)
+
+# is this an income or a bill
+# when did the last one come in?
+# how much?
+# how often do these payments occur
+
+regularFlows = [
+	Cashflow('BIT Scholarship', +576.92, datetime.timedelta(days = 14), datetime.date(2015, 12, 30), datetime.date(2017, 12, 12)),
+	Cashflow('Rent', -246*2, datetime.timedelta(days = 14), datetime.date(2015, 12, 21), datetime.date(2017, 12, 6)),
+	Cashflow('Centrelink', +566.86, datetime.timedelta(days = 14), datetime.date(2015, 12, 24), datetime.date(2016, 6, 1)),
+
+	Cashflow('Living expenses', -246*2 /14, datetime.timedelta(days = 1), datetime.date(2015, 12, 12), datetime.date(2017, 12, 6))
+]
+
+oneoffCashflows = [
+	# ('ANZ', +5206.30, datetime.today()),
+	# ('CUA', +800, datetime.today())
+]
+
+futureOneoffs = []
+
+balance = 5206.30 + 779.78
+
+def getCashflows(untilDate):
+	cashflows = []
+	cashflows += futureOneoffs
+	for cashflow in regularFlows:
+		if cashflow.startDate > untilDate:
+			continue
+
+		nextDateForThisCashflow = cashflow.startDate + cashflow.howOftenReoccur
+		
+		while nextDateForThisCashflow <= untilDate:
+			if nextDateForThisCashflow > date.today():
+				cashflows.append(OneoffCashflow(cashflow.name, cashflow.amount, nextDateForThisCashflow))
+			nextDateForThisCashflow += cashflow.howOftenReoccur
+	
+	return cashflows + oneoffCashflows + futureOneoffs
+
+def disposableCash(date):
+	print(getCashflows(date))
+	return balance + sum(cashflow.amount for cashflow in getCashflows(date))
+
+
+duration = int(sys.argv[1])
+print('Starting balance: $'+str(balance))
+print('Balance in '+str(duration)+' days time is $'+str(disposableCash(date.today() + datetime.timedelta(days = duration))))
